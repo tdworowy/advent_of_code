df16e02e000c7ebd426bd7dddea6a95e100cd8bd
Tomasz Dworowy <dworowytomasz@gmail.com>
2023-12-04 16:41:46 +0100

    update

--- adventOfCode2023/2/advent_of_code_2_rust/src/main.rs
+++ adventOfCode2023/2/advent_of_code_2_rust/src/main.rs
@@ -19,7 +19,7 @@ fn check(value_map: HashMap<&str, usize>) -> bool {
         && value_map.get("green").unwrap() <= &GREEN
 }
 
-fn sum_ids(input: Vec<String>) -> usize {
+fn part1(input: &Vec<String>) -> usize {
     let mut sum: usize = 0;
 
     for s in input {
@@ -55,9 +55,44 @@ fn sum_ids(input: Vec<String>) -> usize {
     sum
 }
 
+fn part2(input: &Vec<String>) -> usize {
+    let mut sum: usize = 0;
+
+    for s in input {
+        // println!("{:?}", s);
+        let chunks: Vec<&str> = s.split(":").collect();
+        // println!("{:?}", chunks[0]);
+        let mut value_map: HashMap<&str, usize> =
+            HashMap::from([("red", 0), ("blue", 0), ("green", 0)]);
+        for set in chunks[1].split(";") {
+            let data: Vec<&str> = set.split(",").collect();
+
+            for d in data {
+                let temp: Vec<&str> = d.split(" ").collect();
+                // println!("{:?} {:?}",id, temp);
+                let v: &str = &temp[1];
+                let t: &str = &temp[2];
+                let mut v_int = v.parse::<usize>().unwrap();
+                if value_map.get_mut(t).unwrap() < &mut v_int {
+                    *value_map.get_mut(t).unwrap() = v_int;
+                }
+            }
+        }
+        sum += value_map.get("red").unwrap()
+            * value_map.get("blue").unwrap()
+            * value_map.get("green").unwrap();
+    }
+
+    sum
+}
+
 fn main() {
     let input = read_lines("input.txt");
-    let sum = sum_ids(input);
-    println!("Part1: {}", sum);
+    let sum1 = part1(&input);
+    println!("Part1: {}", sum1);
+
+    let sum2 = part2(&input);
+    println!("Part2: {}", sum2);
 }
 //part1: 2720
+//part2: 71535
